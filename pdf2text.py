784acd6690bb193f880ff2300c73738ef1ab2ca7
E L Anarcaya Roca <E.Anarcaya-Roca@sheffield.ac.uk>
2024-02-27 11:30:50 +0000

    Refactor variable names for clarity

---
+++ pdf2text.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfReader
+import os
+
+reader = PdfReader(r"G:\My Drive\AMRC Projects\Griding and linishsing automation\Development_of_artificial_neural_network.pdf")
+number_of_pages = len(reader.pages)
+path2save = 'text_output'
+file_name = 'Development_of_artificial_neural_network.txt'
+# read all pages and save as a single txt file
+with open(os.path.join(path2save, file_name), 'w', encoding='utf-8') as f:
+    for i in range(number_of_pages):
+        f.write(reader.pages[i].extract_text())
+        f.write("\n")
+
+
+
+
+
+
+
+
