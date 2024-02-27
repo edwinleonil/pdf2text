ca0fea09608b3cfcea57e8530099597662fa89f7
E L Anarcaya Roca <E.Anarcaya-Roca@sheffield.ac.uk>
2024-02-27 14:19:51 +0000

    Refactor pdf2text.py to improve file naming and readability

--- pdf2text.py
+++ pdf2text.py
@@ -1,10 +1,16 @@
 from PyPDF2 import PdfReader
 import os
 
-reader = PdfReader(r"G:\My Drive\AMRC Projects\Griding and linishsing automation\Development_of_artificial_neural_network.pdf")
+pdf_file_path = r"G:\My Drive\AMRC Projects\Griding and linishsing automation\papers\Predicting_Surface_Roughness_in_Grinding.pdf"
+
+reader = PdfReader(pdf_file_path)
 number_of_pages = len(reader.pages)
+
+# get the name of the file and rename it with a txt extention for the saving name
+file_name = os.path.basename(pdf_file_path).split('.')[0] + '.txt'
+
 path2save = 'text_output'
-file_name = 'Development_of_artificial_neural_network.txt'
+
 # read all pages and save as a single txt file
 with open(os.path.join(path2save, file_name), 'w', encoding='utf-8') as f:
     for i in range(number_of_pages):
