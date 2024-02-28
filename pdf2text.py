a96503acfc76028692943c2d91483c6c35cc4347
E L Anarcaya Roca <E.Anarcaya-Roca@sheffield.ac.uk>
2024-02-28 16:46:22 +0000

    Updated PDF file path and added functionality to add empty line before each line that starts with a number

--- pdf2text.py
+++ pdf2text.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfReader
 import os
 
-pdf_file_path = r"G:\My Drive\AMRC Projects\Griding and linishsing automation\papers\Predicting_Surface_Roughness_in_Grinding.pdf"
+pdf_file_path = r"G:\Shared drives\Quantum Computing - IMG\Research\A Study on the basics of Quantum Computing.pdf"
 
 reader = PdfReader(pdf_file_path)
 number_of_pages = len(reader.pages)
@@ -13,7 +13,31 @@ path2save = 'text_output'
 
 # read all pages and save as a single txt file
 with open(os.path.join(path2save, file_name), 'w', encoding='utf-8') as f:
-    for i in range(number_of_pages):
-        f.write(reader.pages[i].extract_text())
-        f.write("\n")
+    for page in reader.pages:
+        text = page.extract_text()
+        if text.strip():  # Check if the extracted text is not empty or contains only whitespace
+            lines = text.strip().split("\n")  # Split the text into lines
+            for line in lines:
+                if line.strip().isdigit():  # Check if the line contains only digits (page number)
+                    continue  # Skip writing the line if it is a page number
+                f.write(line.strip() + " ")  # Write the stripped line to the file with a space
+                f.write("\n")
 
+print(f"Text extracted from {number_of_pages} pages and saved as {file_name} in {path2save}")
+
+
+# load the text file 
+with open(os.path.join(path2save, file_name), 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    # remove the empty lines 
+    lines = [line.strip() for line in lines if line.strip()]
+    # add empty line before each line that starts with a number
+    updated_lines = []
+    for line in lines:
+        if line.strip().isdigit():  # Check if the line contains only digits (page number)
+            updated_lines.append("")  # Add empty line before the line
+        updated_lines.append(line)  # Keep the line as it is
+    # save the updated text file
+    with open(os.path.join(path2save, file_name), 'w', encoding='utf-8') as f:
+        f.write('\n'.join(updated_lines))
+print(f"Empty line added before each line that starts with a number in {file_name} in {path2save}")
